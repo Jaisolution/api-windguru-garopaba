04a93468f93c6766ba1316965f417fdfcac1aba2
Jaisolution <jaiarduino@gmail.com>
2026-09-10 22:21:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-python
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="API Windguru Garopaba",
-    description="API de previsão para o projeto ESP32",
-    version="1.0.0"
+title="API Windguru Garopaba",
+description="API de previsão para ESP32",
+version="1.0.0"
 )
-
 
 @app.get("/")
 def inicio():
-    return {
-        "status": "online",
-        "api": "Windguru Garopaba",
-        "spot": 209196
-    }
-
+return {
+"status": "online",
+"api": "Windguru Garopaba",
+"spot": 209196
+}
 
 @app.get("/garopaba")
 def garopaba():
-    return {
-        "local": "Garopaba",
-        "windguru_spot": 209196,
-        "previsao": [],
-        "mensagem": "API funcionando. A extração dos dados será adicionada."
-    }
+return {
+"local": "Garopaba",
+"windguru_spot": 209196,
+"previsao": [],
+"mensagem": "API funcionando"
+}
